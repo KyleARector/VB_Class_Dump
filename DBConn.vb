e2bf89e4b199e71f7d7f8410df165444ad0faeda
Kyle Rector <kylearector@outlook.com>
2015-04-15 19:00:04 -0400

    Adding DBConn and ExcelExport classes

---
+++ DBConn.vb
@@ -0,0 +1,73 @@
+﻿'---------------------------------------------------------'
+'--------------------Solution Imports---------------------'
+'---------------------------------------------------------'
+
+'---------------------------------------------------------'
+'---------------------General Imports---------------------'
+'---------------------------------------------------------'
+Imports System.Data.SqlClient
+
+Namespace DBConn
+
+    Public Class DBConnection
+        '---------------------------------------------------------'
+        '------------------------Variables------------------------' x
+        '---------------------------------------------------------'
+        Private _ConnString As String
+        Private _Connection As SqlConnection = Nothing
+        Private _SQLCommand As SqlCommand = Nothing
+        Private _SQLReader As SqlDataReader = Nothing
+        Private _ResultList As List(Of List(Of Object))
+
+        '---------------------------------------------------------'
+        '------------------------Properties-----------------------'
+        '---------------------------------------------------------'
+        Public ReadOnly Property ResultList As List(Of List(Of Object))
+            Get
+                ResultList = _ResultList
+            End Get
+        End Property
+
+        '---------------------------------------------------------'
+        '-------------------------Methods-------------------------'
+        '---------------------------------------------------------'
+        Public Sub New()
+            _ConnString = "Server=YOUR_INFO_HERE;Database=YOUR_INFO_HERE;User Id=YOUR_INFO_HERE;Password=YOUR_INFO_HERE;"
+            _Connection = New SqlConnection
+            _Connection.ConnectionString = _ConnString
+        End Sub
+
+        Public Sub RunQuery(ByVal QueryString As String)
+            _Connection.Open()
+            _SQLCommand = New SqlCommand(QueryString, _Connection)
+            _SQLReader = _SQLCommand.ExecuteReader
+            _ResultList = New List(Of List(Of Object))
+            If _SQLReader.HasRows Then
+                Dim FirstRow As Boolean = True
+                Dim i As Integer = 1
+                Do While _SQLReader.Read
+                    Dim LoopList As List(Of Object)
+                    For j As Integer = 0 To _SQLReader.VisibleFieldCount - 1
+                        If FirstRow = True Then
+                            LoopList = New List(Of Object)
+                            LoopList.Add(_SQLReader.GetValue(j))
+                            _ResultList.Add(LoopList)
+                        Else
+                            LoopList = _ResultList.Item(j)
+                            LoopList.Add(_SQLReader.GetValue(j))
+                        End If
+                    Next
+                    If i = 1 Then
+                        FirstRow = False
+                    End If
+                    i = i + 1
+                Loop
+            End If
+            _SQLReader.Close()
+            _Connection.Close()
+        End Sub
+
+    End Class
+
+End Namespace
+
---
+++ ExcelExport.vb
@@ -0,0 +1,66 @@
+﻿'---------------------------------------------------------'
+'--------------------Solution Imports---------------------'
+'---------------------------------------------------------'
+
+'---------------------------------------------------------'
+'---------------------General Imports---------------------'
+'---------------------------------------------------------'
+Imports Microsoft.Office.Interop.Excel
+Imports System.ComponentModel
+
+Namespace ExcelExport
+
+    Public Class ExcelExport
+        '---------------------------------------------------------'
+        '------------------------Variables------------------------'
+        '---------------------------------------------------------'
+        Private _ExcelApp As Application
+        Private _WrkBk As Workbook
+        Private _WrkSheet As Worksheet
+
+        '---------------------------------------------------------'
+        '------------------------Properties-----------------------'
+        '---------------------------------------------------------'
+
+        '---------------------------------------------------------'
+        '-------------------------Methods-------------------------'
+        '---------------------------------------------------------'
+        Public Sub New()
+            _ExcelApp = New Application
+            _ExcelApp.Visible = False
+        End Sub
+
+        Public Sub RunExport(ByVal FilePath As String, ByVal ObjectList As List(Of Object), ByVal HeaderArray As String())
+            'Be sure to sanitize the file name before use, if not determined by code'
+            _WrkBk = _ExcelApp.Workbooks.Add
+            _WrkSheet = _WrkBk.Worksheets.Item(1)
+
+            'Create column headers based on header array passed in
+            For i As Integer = 1 To HeaderArray.Count
+                _WrkSheet.Cells(1, i) = HeaderArray(i - 1)
+            Next
+            Dim LoopObject As New Object
+            'For every object in the input list, get attributes that match the header for that column, and write to Excel
+            For i As Integer = 1 To ObjectList.Count
+                LoopObject = ObjectList.Item(i - 1)
+                For j As Integer = 1 To HeaderArray.Count
+                    _WrkSheet.Cells(i + 1, j) = GetPropByName(LoopObject, HeaderArray(j - 1))
+                Next
+            Next
+            'Save the workbook to the work directory
+            _WrkBk.SaveAs(FilePath & "yourfilename.xlsx")
+            _WrkBk.Close()
+        End Sub
+
+        'Get the value of an object's attribute by the property's name
+        Private Function GetPropByName(ByVal ParentObject As Object, ByVal PropertyName As String) As Object
+            Dim ObjectType As Type = ParentObject.GetType()
+            Dim PropInfo As System.Reflection.PropertyInfo = ObjectType.GetProperty(PropertyName)
+            Dim PropValue As Object = PropInfo.GetValue(ParentObject, Reflection.BindingFlags.GetProperty, Nothing, Nothing, Nothing)
+            Return PropValue
+        End Function
+
+    End Class
+
+End Namespace
+
